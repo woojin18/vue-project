c3d8444ec8a8ef1cc0483fdc5d3892f4b4e97a0b
woojin <woojin180213@naver.com>
2024-04-14 22:15:41 +0900

    test

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    // Prefetch 플러그인을 비활성화
+    config.plugins.delete('prefetch')
+
+    // 또는 특정 엔트리 포인트에 대한 Prefetch 플러그인을 비활성화
+    config.plugins.delete('prefetch-some-entry-point')
+  }
 })
